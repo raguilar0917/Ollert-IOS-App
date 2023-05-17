27e15c0f734df05250638fdb19d5d00481b87aca
ardevarap <adevarapally@sfsu.edu>
2023-05-16 23:29:14 -0700

    First Commit

---
+++ Ollert_APP/Ollert_APP/Ollert_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ollert_APPApp.swift
+//  Ollert_APP
+//
+//  Created by Aakanksha Devarapally on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Ollert_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
